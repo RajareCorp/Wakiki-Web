b6fa54f73f522d58543e65b8203b15a651030039
Rajare <rajare.corp@gmail.com>
2024-03-21 19:12:14 +0100

    Better Setup

--- wakiki_set_up.bat
+++ wakiki_set_up.bat
@@ -1,9 +1,45 @@
 @echo off
-copy .env.example .env
-php artisan migrate
 setlocal
 
-rem Chemin du fichier cible (à remplacer par le chemin de votre fichier)
+for /f "tokens=*" %%A in ('where php') do set PHPLocation=%%~dpA
+if "%PHPLocation%"=="" (
+    echo PHP n'est pas installe sur ce systeme.
+	pause
+	goto end
+) else (
+    echo PHP est installe a l'emplacement : %PHPLocation%
+)
+
+rem Récupération de l'emplacement de Composer
+for /f "tokens=*" %%A in ('where composer') do set ComposerLocation=%%A
+if "%ComposerLocation%"=="" (
+    echo Composer n'est pas installe sur ce systeme.
+    pause
+	goto end
+) else (
+    echo Composer est installe a l'emplacement : %ComposerLocation%
+)
+
+rem Verification de la presence du fichier php.ini
+if exist "%PHPLocation%\php.ini" (
+    echo Le fichier php.ini est present dans : %PHPLocation%
+) else (
+    echo Le fichier php.ini n'est pas present dans : %PHPLocation%
+	rem Copie du fichier php.ini-development en php.ini
+    copy "%PHPLocation%php.ini-development" "%PHPLocation%php.ini" > nul
+	if exist "%PHPLocation%\php.ini" (
+		echo Le fichier php.ini a ete cree a partir de php.ini-development.
+	) else (
+		echo Le fichier php.ini n'a pas pu etre cree a partir de php.ini-development.
+		pause
+		goto end
+	)
+)
+
+copy .env.example .env
+php artisan migrate
+
+rem Chemin du fichier cible
 set "cible=%~dp0\wakiki.bat"
 
 rem Nom du raccourci et emplacement du bureau
@@ -31,3 +67,7 @@ rem Nettoyer le fichier vbs temporaire
 del CreateShortcut.vbs
 
 endlocal
+
+echo Tout semble en ordre !
+pause
+:end
